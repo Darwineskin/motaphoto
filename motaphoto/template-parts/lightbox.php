a4610d69aa9c20ecb98bd4a46ed441e15860741d
Darwine <alexoggy@hotmail.fr>
2024-09-18 17:21:07 +0200

    lightbox css and responsive

--- motaphoto/template-parts/lightbox.php
+++ motaphoto/template-parts/lightbox.php
@@ -1,11 +1,12 @@
 
 
 <div class="lightbox">
-        <button class="lightbox-close"><img src="<?php echo get_template_directory_uri(); ?>/images/close.png"/></button>
+        <button class="lightbox-close"></button>
 
         <div class="lightbox-container">
-            <button class="lightbox-prev"><img src="<?php echo get_template_directory_uri(); ?>/images/left_arrow.png"/>Précécente</button>
-
+            <button class="lightbox-prev">
+                <span>Précédente</span>
+            </button>
 
             <div class="img-container">
                 <img class="lightbox-img" src="" alt="">
@@ -14,7 +15,9 @@
                     <p class="lightbox-cat"></p>
                 </div>
             </div>
-            <button class="lightbox-next"><img src="<?php echo get_template_directory_uri(); ?>/images/right_arrow.png"/>Suivante</button>
+            <button class="lightbox-next">
+                <span>Suivante</span>
+            </button>
 
         </div>
 
